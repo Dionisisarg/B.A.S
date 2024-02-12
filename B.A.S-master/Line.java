76435229e4b72f812e91a3f9618bd5264213a6cb
Dionisis Marios Argyros <64729197+Dionisisarg@users.noreply.github.com>
2024-02-12 19:39:23 +0200

    Update Line.java

--- B.A.S-master/Line.java
+++ B.A.S-master/Line.java
@@ -1,13 +1,11 @@
 import javax.swing.*;
-import java.io.*;
-import java.util.*; 
 
 public class Line
 
 {
     private int num;
-    private BusStop[] outwardstops=new BusStop[];
-    private BusStop[] returnstops=new BusStop[];
+    private BusStop[] outwardstops;
+    private BusStop[] returnstops;
     public enum TypeOfDir{ OUTWARD,RETURN}
     private TypeOfDir direction;
     //πίνακας δρομολογίων που έχει 7 γραμμές(αντιστοιχούν σε κάθε ημέρα της εβδομάδας) και 9 στήλες(αντιστοιχούν στις χρονικές ζώνες)
@@ -33,31 +31,32 @@ public class Line
 	 boolean[][] routes;
 	 /*Το σύστημα ελέγχει τα στοιχεία της γραμμής προς τη συγκεκριμένη κατεύθηνση και
 	 εμφανίζει στον χρήστη τις ημέρες και ώρες αναχώρησης προς αυτή την κατεύθυνση*/
-	   if( dir == "OUTWARD")
+	   if( dir == TypeOfDir.OUTWARD)
               { routes = this.outwardRoutes; 
-                return routes; }
-              else 
+                }
+           else 
                { routes = this.returnRoutes; 
-                 return routes; }
-		
+                  }
+	   return routes;
 	}
     public BusStop[] getBusStops(TypeOfDir dir)
 
 	{   BusStop[] stops;
 	    //Το σύστημα ανακτά τις στάσεις της γραμμής προς την συγκεκριμένη κατεύθηνση
-            if( dir == "OUTWARD")
+            if( dir == TypeOfDir.OUTWARD)
               { stops = this.outwardstops; 
-                return stops; }
-              else 
+                 }
+            else 
                { stops = this.returnstops; 
-                 return stops; }
+                  }
+	    return stops;
 	}
     public void getDirections()
      {  
          //Το σύστημα εμφανίζει στον χρήστη την επιλογή μεταξύ 2 κατευθύνσεων που υποστηρίζει αυτή η γραμμή.
          System.out.println(" You can go: ");
-         JButton a = new JButton("outward of starting point");
-         JButton a = new JButton("return to starting point");
+         JButton a1 = new JButton("outward of starting point");
+         JButton a2 = new JButton("return to starting point");
 	
      }
     
