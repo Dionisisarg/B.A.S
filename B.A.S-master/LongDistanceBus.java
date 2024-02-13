b3a9e3df4c5851b742aa1d043b9cc057184c0403
Dionisis Marios Argyros <64729197+Dionisisarg@users.noreply.github.com>
2024-02-13 16:26:55 +0200

    Update LongDistanceBus.java

--- B.A.S-master/LongDistanceBus.java
+++ B.A.S-master/LongDistanceBus.java
@@ -1,33 +1,38 @@
 public class LongDistanceBus extends Bus
 {
 	//attributes
-	public enum Type{ MiniBus,DoubledeckerBus, 20-50Seats,50-62Seats }
+	public enum Type{ MiniBus,DoubledeckerBus, Seats_20_50, Seats_50_62 }
 	private Type busType;
+	private int id;
+
+	public int getId(){
+		return id;
+	}
     
 	//constructor
 	public LongDistanceBus(int id, String name, int numSeats, Seat[] busSeats,
-	double[] location,Type type)
+	double[] location, int speed, Type type)
 	{  
-		super(id,name,numSeats,busSeats,location);
+		super(id,name,numSeats,busSeats,location,speed);
 		this.busType = type;
 	}
    
 	public String getBusData()
 	{
 		String freeSeats = "";
-		for (int i = 0; i <= Seats.length; i++) {
-			if (Seat.availability == true)
+		for (int i = 0; i <= busSeats.length; i++) {
+			if (busSeats.isAvailable())
 			{
-				freeSeats = freeSeats + Seats[i].id;	
+				freeSeats += busSeats[i].getSeatId();	
 			}
 		}
 	
-		return "O κωδικός λεωφορείου είναι: " + id + "και οι ελεύθερες θέσεις: " + freeSeats;
+		return "O κωδικός λεωφορείου είναι: " + getId() + "και οι ελεύθερες θέσεις: " + freeSeats;
 	}
 	
 	public Boolean checkSeat(Seat seat)
 	{
-		if (seat.availability == true)
+		if (seat.isAvailable())
 		{
 			return true;
 		}
